5d4a49e32fa3643160d77258cb2293e36deea34b
Nico Bellante <nico.bellante@snowflake.com>
2025-03-19 11:30:48 -0700

    first pass at sections

--- frontend/app/src/components/AppView/AppView.tsx
+++ frontend/app/src/components/AppView/AppView.tsx
@@ -346,6 +346,7 @@ function AppView(props: AppViewProps): ReactElement {
           style={{
             overflow: "hidden",
             width: "100%",
+            height: "100vh",
           }}
         >
           <Header
